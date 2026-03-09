6c8721caefabc98b37b2b669ed737b0f7a3e0428
Abkash Gangwar <abkashgangwar@outlook.com>
2026-03-09 14:36:56 +0530

    Add Eureka server and service discovery configuration

--- eureka-server/src/main/java/com/otp_system/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/otp_system/eureka_server/EurekaServerApplication.java
@@ -1,15 +1,15 @@
-package com.otp.gateway;
+package com.otp_system.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
-public class GatewayApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
